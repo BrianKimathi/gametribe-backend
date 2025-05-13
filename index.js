d2d3532733b40019a16abe9aa6e5df219757d53e
Brian <kimathibrian71@gmail.com>
2025-05-13 11:58:59 +0300

    Bugs fixes and clans updates

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors({ origin: "http://localhost:5173" }));
+// multiple origins
+app.use(cors({ origin: "https://hub.gametribe.com" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
